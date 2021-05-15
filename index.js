fc08fec15ea871c0b2e60bf90bdcafd487d94ba4
marlenevanerp <marlene.van.erp@hva.nl>
2021-05-15 20:56:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-let ejs = require('ejs');
-
+const port = 3001;
 
 // static files 
 app.use(express.static('static')) //de static files worden opgevraagd 
@@ -13,17 +10,20 @@ app.use('style.css', express.static(__dirname + 'static/style.css'))
 app.set('views','./views')
 app.set('view engine', 'ejs')
 
-app.get('views',(req, res) => {
-    res.render('profiel.ejs', {})
+app.get('/',(req, res) => {
+    res.render('index.ejs', {})
+})
+app.get('/about',(req, res) => {
+  res.render('about.ejs', {})
 })
 
-// liston on port 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 
 // Error 404
 app.use(function(req,res){
 	res.status(404);
 	res.render('404')
+})
+// liston on port 3000
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
