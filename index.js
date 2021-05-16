b48cf1a6575ecfc7f0504de4f5c5f4bdc1bb90a1
marlenevanerp <marlene.van.erp@hva.nl>
2021-05-16 20:04:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express')
+const bodyParser =require('body-parser')
+const {MongoClient} = require('mongodb');
+
 const app = express()
-const port = 3001;
+const port = 3006;
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
 
 // static files 
 app.use(express.static('static')) //de static files worden opgevraagd 
 app.use('style.css', express.static(__dirname + 'static/style.css'))
 
-// set Views
-app.set('views','./views')
+// set Templating Enginge
 app.set('view engine', 'ejs')
+let ejs = require('ejs');
+
+
+
+// Set views
+app.set('views','./views')
 
 app.get('/',(req, res) => {
-    res.render('index.ejs', {})
+    res.render('home.ejs', {})
 })
+
 app.get('/about',(req, res) => {
   res.render('about.ejs', {})
 })
 
+app.post('/about', urlencodedParser, (req, res)=> {
+  res.json(req.body)
+
+})
 
 // Error 404
 app.use(function(req,res){
